7f740fbbb6f32f7720dd9c8450881851ca75cfcf
Ashley Winter <ashley.winter2007@gmail.com>
2022-08-19 10:06:42 -0700

    first commit

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 if DEBUG:
-    STATICFILES_DIRS = [OS.PATH.JOIN(BASE_DIR, "static")]
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 else:
     STATIC_ROOT = [os.path.join(BASE_DIR, "static")]
 
